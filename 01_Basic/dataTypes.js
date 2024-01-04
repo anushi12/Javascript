340632425509ece0072f5c2f57df61ed99a24cff
anushi12 <113124216+anushi12@users.noreply.github.com>
2024-01-04 07:02:39 +0000

    Data types

---
+++ 01_Basic/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//<-----primitive datatypes---->
+/*
+    number => 2 to pow 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined
+    symbol => unique
+*/
+
+//<-------Non - premitive datatype------>
+/*
+   object
+ */
+
+//typeof => is used to find the type of input
+
+console.log(typeof "Anushi") // string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
