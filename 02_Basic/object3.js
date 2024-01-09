c71fab977c3a9072d6c8979c1f790dfc8cc5542e
anushi12 <113124216+anushi12@users.noreply.github.com>
2024-01-09 07:48:13 +0000

    Object destructuring edited

--- 02_Basic/object3.js
+++ 02_Basic/object3.js
@@ -8,7 +8,27 @@ const course = {
 // const.coursename
 
 const {instructor : teacher} = course
-console.log(instructor) // self
-console.log(teacher) //self
+//console.log(instructor) // self
+//console.log(teacher) //self
+
+//destructing a null object
+
+function getPerson(){
+    return null;
+}
+//console.log(getPerson()) // null
+
+// const {firstname, lastname} = getPerson()
+// console.log(firstname,lastname) // TypeError
+
+// TypeError : arises when it cannot destructure property as it is null
+
+// solution 
+const {firstname, lastname} = getPerson() || {}
+console.log(firstname,lastname) // TypeError 
+// undefined undefined 
+
+
+
 
 
