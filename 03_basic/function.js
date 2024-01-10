5bb442dd6f0d1c75c246afd176894798a57f7459
anushi12 <113124216+anushi12@users.noreply.github.com>
2024-01-10 12:42:12 +0000

    functions

--- 03_basic/function.js
+++ 03_basic/function.js
@@ -16,3 +16,45 @@ function loginUserName(username){
 }
 console.log(loginUserName("Anushi"))
 console.log(loginUserName()) //undefined
+
+
+// rest ans spread opertator
+
+function createOrderList(...num){
+    return num
+}
+//without ...num
+console.log(createOrderList(200, 400, 589, 3685)) // [200]
+
+// to get all values use rest (...)
+console.log(createOrderList(200, 400, 589, 3685)) // [200,400, 589, 3685]
+
+// similarly we can do it for objects
+
+const user = {
+    username: "Anushi",
+    hobby : "learning"
+}
+// to pass objects in function
+function handleObj(anyobj){
+     console.log(`username is ${anyobj.username} and hobby is ${anyobj.hobby}`)
+}
+handleObj(user)
+
+// alternate way
+
+handleObj({
+    username: "Bunny",
+    hobby : "learning"
+})
+
+// for arrays
+
+const myArr = [200,300,456,7895]
+
+function returnValue(getArr){
+    return getArr[3]
+}
+console.log(returnValue(myArr)) // 7895
+// alternate way
+console.log(returnValue([200,300,456,7895])) // 7895
