3008aca19001f549080702358d2492981ce62f5d
anushi12 <113124216+anushi12@users.noreply.github.com>
2024-01-21 09:07:10 +0000

    while and do-while

---
+++ 04_iterations/while.js
@@ -0,0 +1,28 @@
+// let i = 0;
+// while(i<=10){
+//     console.log(i);
+//     i = i+2;
+// }
+
+
+//for arrays
+
+// const arr = ["thor", "hulk", "spiderman"]
+// let i =0
+// let n = arr.length
+// console.log(n)
+// while(i<n){
+//     console.log(arr[i])
+//     i++
+// }
+
+
+// do-while loop
+// this loop will execute at least once even if the condition is false
+
+const scores = ["100","200","300"]
+let i = 0
+do {
+    console.log(scores[i])
+    i++
+} while (i < scores.length);
